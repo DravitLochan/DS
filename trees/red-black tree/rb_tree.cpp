0b78503a05747ca75169dac20fe39c0ed83a66c2
DravitLochan <dlochangupta@gmail.com>
2017-02-02 19:57:17 +0530

    basic structure is ready

--- trees/red-black tree/rb_tree.cpp
+++ trees/red-black tree/rb_tree.cpp
@@ -6,7 +6,7 @@ struct rb_node
 	int val;
 	char c;					//c = color of node. n = no color, r = red, b = black//
 	struct rb_node *left,*right,*parent;
-}
+};
 
 class RbTree
 {
@@ -19,9 +19,9 @@ class RbTree
 			return root;
 		}
 		
-		void setRoot(rb_node * root)
+		void setRoot(rb_node * r)
 		{
-			this.root=root;
+			root=r;
 		}
 		
 		void createNode(int val)
@@ -69,30 +69,71 @@ class RbTree
 		
 		void solveRedRed(rb_node *ptr)
 		{								
-			if(ptr->parent==ptr->parent->parent->left)							//condition to find if ptr's parent is left or right child of it's gand parent
+			if(ptr->parent==ptr->parent->parent->left)							//condition to find if ptr's parent is left child of it's gand parent
 			{
-				if()															//ptr's uncle doe not exist
+				if(ptr->parent->parent->right==NULL)															//ptr's uncle doe not exist.
 				{
-					
+					/*check for 2 conditions. i.e. ll,rl (only 2 possible 
+					cases as we have found from above that parent is left 
+					child of ptr's grand parent)*/
+					if(ptr==ptr->parent->left)																	//ll case
+						rightRotate(ptr);
+					else if(ptr==ptr->parent->right)															//rl case
+						leftRightRotate(ptr);					
 				}
-				else if(ptr->parent->parent->left->c=='r')						//ptr's uncle is red
-				{														//here error will come for those cases where ptr->parent->parent does not even exits						
+				else if(ptr->parent->parent->right->c=='r')														//ptr's uncle is red
+				{														
 					ptr->parent->c='b';
 					ptr->parent->parent->right->c='b';
 					ptr->parent->parent->c='r';
 					//make a recursive call to check ptr->parent->parent
 				}
-				else if()														//ptr's uncle is black
+				else if(ptr->parent->parent->right->c='b')														//ptr's uncle is black
 				{
-					
+					//similar to above, check for ll or rl case and rotate
+					/*check for 2 conditions. i.e. ll,rl (only 2 possible 
+					cases as we have found from above that parent is left 
+					child of ptr's grand parent)*/
+					if(ptr==ptr->parent->left)																	//ll case
+						rightRotate(ptr);
+					else if(ptr==ptr->parent->right)															//rl case
+						leftRightRotate(ptr);					
 				}
 			}
-			else if(ptr==ptr->parent->right)									//condition to find if ptr->parent is left or right child of it's grand parent
+			else if(ptr->parent==ptr->parent->parent->right)					//condition to find if ptr's parent is right child of it's grand parent
 			{
-								//~ to above, check all the conditions here also
+				//~ to above, check all the conditions here also
+				if(ptr->parent->parent->left==NULL)															//ptr's uncle doe not exist.
+				{
+					/*check for 2 conditions. i.e. rr,lr (only 2 possible 
+					cases as we have found from above that parent is left 
+					child of ptr's grand parent)*/
+					if(ptr==ptr->parent->left)																	//lr case
+						rightLeftRotate(ptr);
+					else if(ptr==ptr->parent->right)															//rr case
+						leftRotate(ptr);					
+				}
+				else if(ptr->parent->parent->left->c=='r')														//ptr's uncle is red
+				{														
+					ptr->parent->c='b';
+					ptr->parent->parent->left->c='b';
+					ptr->parent->parent->c='r';
+					//make a recursive call to check ptr->parent->parent
+				}
+				else if(ptr->parent->parent->left->c='b')														//ptr's uncle is black
+				{
+					//similar to above, check for ll or rl case and rotate
+					/*check for 2 conditions. i.e. ll,rl (only 2 possible 
+					cases as we have found from above that parent is left 
+					child of ptr's grand parent)*/
+					if(ptr==ptr->parent->left)																	//lr case
+						rightLeftRotate(ptr);
+					else if(ptr==ptr->parent->right)															//rr case
+						leftRotate(ptr);
+				}
 			}
 		}
-}
+};
 
 int main()
 {
