787f637a659a716ca421cd62ed25cc9768b3b433
DravitLochan <dlochangupta@gmail.com>
2017-04-26 07:43:48 +0530

    Longest Increasing Sequence in N^2

--- DP/lis.cpp
+++ DP/lis.cpp
@@ -1,21 +1,41 @@
 #include<iostream>
 using namespace std;
-
-int lis(int arr[], int indices[], int size, int i)
+//{10 22 9 33 21 50 41 60};
+//{22 9 10 11 12 13 14 15 16 17 18 19 20 21 22 33 50 41 60}
+int lis(int arr[], int indices[], int size)
 {
-	if(i == 0)
+	int i, j, lis;
+	for( i = 0; i < size; ++i)
 	{
-		indices[i] = 1;
+		//cout<<arr[i]<<"-> ";
+		for (j = 0; j < i; ++j)
+		{
+			if( arr[j] < arr[i] )
+			{
+				lis = indices[i];
+				int a = indices[j] + 1;
+				indices[i] = a > lis ? a : lis;
+				//cout << indices[i];
+			}
+		}
+		//cout<<"\n";
 	}
-	if()
+	lis = indices[0];
+	for( i = 1; i < size; ++i)
+		lis = indices[i] > lis ? indices[i] : lis;
+	return lis;	
 }
 
 int main()
 {
 	int i, n;
 	cin>>n;
-	int arr[n], indices[n];
+	int arr[n], indices[n] ;
 	for (i=0; i<n; ++i)
+	{
 		cin>>arr[i];
-	cout<<lis(arr, indices, n, 0);	
+		indices[i]=1;
+	}
+	cout<<lis(arr, indices, n);	
+	return 0;
 }
