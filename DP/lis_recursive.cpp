2009a5b6f7b7d90ff2e3c9cce87b907bc0ce1ea6
DravitLochan <dlochangupta@gmail.com>
2017-04-30 22:34:56 +0530

    lis recursive (buggy)

---
+++ DP/lis_recursive.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+//{10 22 9 33 21 50 41 60};
+//{22 9 10 11 12 13 14 15 16 17 18 19 20 21 22 33 50 41 60}
+
+int lis(int arr[], int n)
+{
+	static int max_len = 1;
+	if(n == 1)
+	{
+		return 1;
+	}
+	int cur_len = 1;
+	for(int i = 1; i < n; ++i)
+	{
+		int len = lis(arr, i);
+		if((len + 1 > cur_len) && (arr[i]<arr[n-1]))
+			cur_len = len + 1;	
+	}
+	if(cur_len > max_len)
+		max_len = cur_len ;
+	cout<<max_len<<"\n";
+	return max_len;	
+}
+
+int main()
+{
+	int i, n;
+	cin>>n;
+	int arr[n];
+	for (i=0; i<n; ++i)
+		cin>>arr[i];
+	cout<<lis(arr, n);	
+	return 0;
+}
