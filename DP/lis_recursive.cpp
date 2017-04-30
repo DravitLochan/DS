ff72108015ef451926a0a09d62e6252975ea8935
DravitLochan <dlochangupta@gmail.com>
2017-04-30 23:02:52 +0530

    bugs

--- DP/lis_recursive.cpp
+++ DP/lis_recursive.cpp
@@ -4,24 +4,30 @@ using namespace std;
 //{10 22 9 33 21 50 41 60};
 //{22 9 10 11 12 13 14 15 16 17 18 19 20 21 22 33 50 41 60}
 
-int lis(int arr[], int n)
+int lis(int arr[], int n, int *max_len)
 {
-	static int max_len = 1;
 	if(n == 1)
 	{
 		return 1;
 	}
 	int cur_len = 1;
-	for(int i = 1; i < n; ++i)
+	for(int i = 0; i < n-1; ++i)
 	{
-		int len = lis(arr, i);
+		int len = lis(arr, i, max_len);
 		if((len + 1 > cur_len) && (arr[i]<arr[n-1]))
 			cur_len = len + 1;	
 	}
-	if(cur_len > max_len)
-		max_len = cur_len ;
-	cout<<max_len<<"\n";
-	return max_len;	
+	if(cur_len > *max_len)
+		*max_len = cur_len ;
+	cout<<*max_len<<"\n";
+	return cur_len;	
+}
+
+int _lis(int arr[], int n)
+{
+	int max_len = 1; 
+	lis(arr, n, &max_len);
+	return max_len;
 }
 
 int main()
@@ -31,6 +37,6 @@ int main()
 	int arr[n];
 	for (i=0; i<n; ++i)
 		cin>>arr[i];
-	cout<<lis(arr, n);	
+	cout<<_lis(arr, n);	
 	return 0;
 }
