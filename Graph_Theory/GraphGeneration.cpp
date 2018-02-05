5c7b5173bb1398d840787304c8ee9d84184a89c7
DravitLochan <dlochangupta@gmail.com>
2018-02-05 14:02:18 +0530

    user input based graph generation

--- Graph_Theory/GraphGeneration.cpp
+++ Graph_Theory/GraphGeneration.cpp
@@ -10,31 +10,40 @@ using namespace std;
 class Graph{
 	
 	public:
-	int size;
+	int v, e;
 	vector< vector<int> > adjecency_list;
 	
-	Graph(int size) {
-		this->size = size;
-		adjecency_list.resize(size);
+	Graph(int v, int e) {
+		this->v = v;
+		this->e = e;
+		adjecency_list.resize(v);
 	}
 	
+		// undirected graph, so insert twice
+		
 	void addEdge(int v1, int v2) {
 		this->adjecency_list[v1].push_back(v2);
+		this->adjecency_list[v2].push_back(v1);
 	}
 	
 };
 
 int main() {
 
-	int size;
-	cin>>size;
-	Graph g = Graph(size);
-	g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
-    g.addEdge(3, 3);
+	int v, e, i, v1, v2;
+	cout<<"enter number of vertices \n";
+	cin>>v;
+    cout<<"enter number of edges \n";
+    cin>>e;
+    Graph g = Graph(v, e);
+    i = 0;
+    cout<<"Enter vertices in pair for an edge \n";
+    while(i<e) {
+    	cin>>v1>>v2;
+    	g.addEdge(v1, v2);
+    	++i;
+	}
+	
 	return 0;
 
 }
